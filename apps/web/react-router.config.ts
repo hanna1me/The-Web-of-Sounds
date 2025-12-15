1dd77db801a6e243c9b01e520dbf0304b7935acf
hanna1me <hchang@macalester.edu>
2025-12-15 15:57:22 -0600

    2

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -1,9 +1,8 @@
 import type { Config } from "@react-router/dev/config";
-import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
   appDirectory: "./src/app",
-  ssr: true,
-  // prerender: ['/*?'],  // <-- remove this
+  ssr: false,
+  // remove prerender entirely
 } satisfies Config;
 
